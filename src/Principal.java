f77df4d59c5bdcfe660ee5ae77c5eff48b715846
Rolando Roque Kuan <rroquek@msn.com>
2024-04-10 14:55:42 -0600

    primer commit

--- src/Principal.java
+++ src/Principal.java
@@ -7,15 +7,25 @@ public class Principal
         miPelicula.fechaDeLanzmiento=2021;
         miPelicula.duracionEnMinutos=120;
 
-        System.out.printf("Mi pelicula es %s%n", miPelicula.nombre);
-        System.out.printf("Su fecha de lanzamiento es: %d%n", miPelicula.fechaDeLanzmiento);
+        miPelicula.muestraIchaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+
+        System.out.printf("La Evaluacion es %f \n\n",miPelicula.sumaDeLasEvaluaciones);
+        System.out.printf("Total de las evaluaciones %f\n\n,", miPelicula.totalDeLasEvaluaciones);
+        System.out.printf("%f\n\n",miPelicula.calculaMedia());
+
+//        System.out.printf("Mi pelicula es %s%n", miPelicula.nombre);
+//        System.out.printf("Su fecha de lanzamiento es: %d%n", miPelicula.fechaDeLanzmiento);
 
         Pelicula otraPelicula = new Pelicula();
         otraPelicula.nombre="Matrix";
         otraPelicula.fechaDeLanzmiento=1998;
 
-        System.out.printf("Mi pelicula es %s%n", otraPelicula.nombre);
-        System.out.printf("Su fecha de lanzamiento es: %d%n", otraPelicula.fechaDeLanzmiento);
+        otraPelicula.muestraIchaTecnica();
+
+//        System.out.printf("Mi pelicula es %s%n", otraPelicula.nombre);
+//        System.out.printf("Su fecha de lanzamiento es: %d%n", otraPelicula.fechaDeLanzmiento);
 
 
 
