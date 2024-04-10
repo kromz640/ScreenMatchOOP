ed35b2372dcaa7f9369a0723b4b5ad5acdaa8193
Rolando Roque Kuan <rroquek@msn.com>
2024-04-10 16:49:31 -0600

    primer commit

--- src/Principal.java
+++ src/Principal.java
@@ -1,36 +1,17 @@
-public class Principal
-{
-    public static void main(String[] args)
-    {
-        Pelicula miPelicula= new Pelicula();
-        miPelicula.nombre="Encanto";
-        miPelicula.fechaDeLanzmiento=2021;
-        miPelicula.duracionEnMinutos=120;
+import com.aluracursos.screenmatch.modelos.Pelicula;
 
-        miPelicula.muestraIchaTecnica();
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.setNombre("La familia del futuro");
+        miPelicula.setFechaDeLanzamiento(2007);
+        miPelicula.setDuracionEnMinutos(95);
+        miPelicula.setIncluidoEnElPlan(true);
+
+        miPelicula.muestraFichaTecnica();
         miPelicula.evalua(10);
         miPelicula.evalua(10);
-
-        System.out.printf("La Evaluacion es %f \n\n",miPelicula.sumaDeLasEvaluaciones);
-        System.out.printf("Total de las evaluaciones %f\n\n,", miPelicula.totalDeLasEvaluaciones);
-        System.out.printf("%f\n\n",miPelicula.calculaMedia());
-
-//        System.out.printf("Mi pelicula es %s%n", miPelicula.nombre);
-//        System.out.printf("Su fecha de lanzamiento es: %d%n", miPelicula.fechaDeLanzmiento);
-
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre="Matrix";
-        otraPelicula.fechaDeLanzmiento=1998;
-
-        otraPelicula.muestraIchaTecnica();
-
-//        System.out.printf("Mi pelicula es %s%n", otraPelicula.nombre);
-//        System.out.printf("Su fecha de lanzamiento es: %d%n", otraPelicula.fechaDeLanzmiento);
-
-
-
-
-
-
+        miPelicula.evalua(7.8);
+        System.out.println("Média de evaluaciones de la película: " +miPelicula.calculaMedia());
     }
 }
